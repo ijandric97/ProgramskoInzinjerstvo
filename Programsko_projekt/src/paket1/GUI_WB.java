df66a7070801c0e5335de6f9d387d28619c08e2c
erdo9 <48565082+erdo9@users.noreply.github.com>
2019-06-07 17:38:45 +0200

    Add files via upload

--- Programsko_projekt/src/paket1/GUI_WB.java
+++ Programsko_projekt/src/paket1/GUI_WB.java
@@ -22,7 +22,7 @@ public class GUI_WB implements ActionListener {
 	private JTextField textField;
 	private JButton btnPotrai;
 	private JCheckBox chckbxNewCheckBox;
-	private JCheckBox checkBox;
+	private JCheckBox chckbxEventClearanceGroup;
 	private JCheckBox chckbxNewCheckBox_1;
 	private JCheckBox chckbxNewCheckBox_2;
 	private JCheckBox chckbxNewCheckBox_3;
@@ -32,8 +32,8 @@ public class GUI_WB implements ActionListener {
 	private JTable table;
 	
 	private String[] columnNames = {"Rule", "Support", "Confidence", "Lift"};
-	private JCheckBox checkBox_1;
-	private JCheckBox checkBox_2;
+	private JCheckBox chckbxNista;
+	private JCheckBox chckbxNista_1;
 	private JButton btnAnaliziraj;
 	
 	private String data_path;
@@ -58,67 +58,67 @@ public class GUI_WB implements ActionListener {
 		
 		if (chckbxNewCheckBox.isSelected()) {
 			
-			retval += "7";
+			retval += "6";
 			status = true;
 		}
 		
-		if (checkBox.isSelected()) {
+		if (chckbxEventClearanceGroup.isSelected()) {
 			
-			if (!status) retval += "6";
-			else retval += ", 6";
+			if (!status) retval += "7";
+			else retval += ", 7";
 			status = true;
 		}
 		
 		if (chckbxNewCheckBox_1.isSelected()) {
-			
-			if (!status) retval += "5";
-			else retval += ", 5";
-			status = true;
-		}
-		
-		if (chckbxNewCheckBox_2.isSelected()) {
 			
 			if (!status) retval += "8";
 			else retval += ", 8";
 			status = true;
 		}
 		
-		if (chckbxNewCheckBox_3.isSelected()) {
+		if (chckbxNewCheckBox_2.isSelected()) {
 			
 			if (!status) retval += "9";
 			else retval += ", 9";
 			status = true;
 		}
 		
-		if (chckbxNewCheckBox_4.isSelected()) {
+		if (chckbxNewCheckBox_3.isSelected()) {
 			
 			if (!status) retval += "10";
 			else retval += ", 10";
 			status = true;
 		}
 		
-		if (chckbxNewCheckBox_5.isSelected()) {
+		if (chckbxNewCheckBox_4.isSelected()) {
 			
 			if (!status) retval += "11";
 			else retval += ", 11";
 			status = true;
 		}
 		
-		if (chckbxNewCheckBox_6.isSelected()) {
+		if (chckbxNewCheckBox_5.isSelected()) {
 			
 			if (!status) retval += "12";
 			else retval += ", 12";
 			status = true;
 		}
 		
-		if (checkBox_1.isSelected()) {
+		if (chckbxNewCheckBox_6.isSelected()) {
+			
+			if (!status) retval += "4";
+			else retval += ", 4";
+			status = true;
+		}
+		
+		if (chckbxNista.isSelected()) {
 			
 			if (!status) retval += "15";
 			else retval += ", 15";
 			status = true;
 		}
 		
-		if (checkBox_2.isSelected()) {
+		if (chckbxNista_1.isSelected()) {
 			
 			if (!status) retval += "";
 			else retval += ", ";
@@ -187,45 +187,45 @@ public class GUI_WB implements ActionListener {
 		
 		btnPotrai.addActionListener(this);
 		
-		chckbxNewCheckBox = new JCheckBox("Event Clearance Group");
+		chckbxNewCheckBox = new JCheckBox("Event Clearance Subgroup");
 		chckbxNewCheckBox.setBounds(24, 57, 235, 23);
 		desktopPane.add(chckbxNewCheckBox);
 		
-		checkBox = new JCheckBox("Event Clearance Subgroup");
-		checkBox.setBounds(24, 84, 235, 23);
-		desktopPane.add(checkBox);
+		chckbxEventClearanceGroup = new JCheckBox("Event Clearance Group");
+		chckbxEventClearanceGroup.setBounds(24, 84, 235, 23);
+		desktopPane.add(chckbxEventClearanceGroup);
 		
-		chckbxNewCheckBox_1 = new JCheckBox("Event Clearance Description");
+		chckbxNewCheckBox_1 = new JCheckBox("Event Clearance Date");
 		chckbxNewCheckBox_1.setBounds(24, 111, 235, 23);
 		desktopPane.add(chckbxNewCheckBox_1);
 		
-		chckbxNewCheckBox_2 = new JCheckBox("Event Clearance Date");
+		chckbxNewCheckBox_2 = new JCheckBox("Hundred Block Location");
 		chckbxNewCheckBox_2.setBounds(24, 138, 235, 23);
 		desktopPane.add(chckbxNewCheckBox_2);
 		
-		chckbxNewCheckBox_3 = new JCheckBox("Hundred Block Location");
+		chckbxNewCheckBox_3 = new JCheckBox("District / Sector");
 		chckbxNewCheckBox_3.setBounds(24, 165, 235, 23);
 		desktopPane.add(chckbxNewCheckBox_3);
 		
-		chckbxNewCheckBox_4 = new JCheckBox("District / Sector");
+		chckbxNewCheckBox_4 = new JCheckBox("Zone / Beat");
 		chckbxNewCheckBox_4.setBounds(24, 192, 235, 23);
 		desktopPane.add(chckbxNewCheckBox_4);
 		
-		chckbxNewCheckBox_5 = new JCheckBox("Zone / Beat");
+		chckbxNewCheckBox_5 = new JCheckBox("Census / Tract");
 		chckbxNewCheckBox_5.setBounds(24, 219, 235, 23);
 		desktopPane.add(chckbxNewCheckBox_5);
 		
-		chckbxNewCheckBox_6 = new JCheckBox("Census / Tract");
+		chckbxNewCheckBox_6 = new JCheckBox("Event Clearance Code");
 		chckbxNewCheckBox_6.setBounds(24, 246, 235, 23);
 		desktopPane.add(chckbxNewCheckBox_6);
 		
-		checkBox_1 = new JCheckBox("Incident Location");
-		checkBox_1.setBounds(24, 273, 235, 23);
-		desktopPane.add(checkBox_1);
+		chckbxNista = new JCheckBox("Nista");
+		chckbxNista.setBounds(24, 273, 235, 23);
+		desktopPane.add(chckbxNista);
 		
-		checkBox_2 = new JCheckBox("New check box");
-		checkBox_2.setBounds(24, 300, 235, 23);
-		desktopPane.add(checkBox_2);
+		chckbxNista_1 = new JCheckBox("Nista");
+		chckbxNista_1.setBounds(24, 300, 235, 23);
+		desktopPane.add(chckbxNista_1);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(281, 64, 731, 323);
